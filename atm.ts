f6ff3ee42ead6a92faae40890a19a45e3495e802
“[SidraAsfand]” <[smartgirlbarbie28@gmail.com]>
2024-03-29 09:54:03 -0700

    atm

---
+++ atm.ts
@@ -0,0 +1,78 @@
+#! /usr/bin/env  node
+import inquirer from "inquirer";
+
+let mybalance=25000;//Dollar
+let mypinNum=  42345;
+//get pinNum from user
+ let myAns =  await inquirer.prompt([
+  {
+  name: "pinNum",
+  message: "Enter Your PinNumber",
+  type: "number"
+}
+]
+);
+//using condition for next step
+if ( myAns.pinNum===mypinNum){
+  console.log("valid pin number");
+  let Operations=  await inquirer.prompt([
+    {
+    name: "Operation",
+    message: "select any  one",
+    type: "list",
+    choices: ["withdraw","checkBalance","fastCash"]
+  }
+]);
+//if user Select Withdraw
+if(Operations.Operation=== "withdraw"){
+  let amountAns=  await inquirer.prompt([
+    {
+    name: "Amount",
+    message: "Enter Amount",
+    type: "number"
+  
+  }
+])
+
+
+ if(amountAns.Amount<= mybalance){
+  mybalance-=amountAns.Amount;
+ 
+  
+  console.log(`your current balance is :" ${mybalance }`);
+  
+}
+else {
+  console.log("insuffiecient balance")
+}
+}
+//if user Select Withdraw
+else if(Operations.Operation==="checkBalance" ){
+  
+  console.log(`your current amount is: ${mybalance}`)
+
+}
+//if user Select Withdraw
+else if(Operations.Operation=== "fastCash"){
+  let cashCount=  await inquirer.prompt([
+    {
+    name: "Cash",
+    message: "Choose the desire  amount you want  to WithDraw!",
+    type: "list",
+    choices:["50000","10000","15000","20000","25000"]
+  
+  }
+])
+if (cashCount.Cash<=mybalance){
+  mybalance-=cashCount.Cash
+
+  console.log(`Your Transaction  ${cashCount.Cash}  Successsful!! \n your remaining Balance is: ${mybalance}`)
+}
+
+}
+
+}
+//if user select invalid pinNumber
+else{
+  console.log("invalid pin number please enter correct pin number:")
+}
